528bc6b3349dd772387d8cd847f8d08a8a3fac21
Juan Diego Duque <diegoduqqe@gmail.com>
2025-11-19 18:15:40 -0500

    feat. init project

---
+++ applications/app-service/src/main/java/co/com/management/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.management.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.management.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
