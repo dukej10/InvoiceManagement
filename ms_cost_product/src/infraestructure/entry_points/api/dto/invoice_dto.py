31e85539a85fed6f18d184ed47595f9f01dfd147
Juan Diego Duque <diegoduqqe@gmail.com>
2025-11-21 19:00:07 -0500

    feat: changed name of folder

--- ms_cost_product/src/infraestructure/entry_points/api/dto/invoice_dto.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from infraestructure.entry_points.api.dto.product_dto import ProductDTO
-from pydantic import BaseModel, Field, validator
-from uuid import UUID
-
-class InvoiceRequestDTO(BaseModel):
-    code: UUID = Field(..., description="Código de la factura")
-    products: List[ProductDTO] = Field(..., min_items=1, description="Lista de productos")
