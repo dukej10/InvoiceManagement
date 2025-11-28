3b8d9010dfb8122401805dd2eee81c70c987eedd
Juan Diego Duque <diegoduqqe@gmail.com>
2025-11-28 15:04:47 -0500

    feat: add strucutre of credit management

---
+++ ms_credit_management/applications/app-service/src/main/java/co/com/credit/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.credit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.credit.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
